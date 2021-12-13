de6d8949287baf79097007fb6573d56fbf36b71e
Angus Hsu <angus@emq.com>
2021-12-13 17:56:19 +0800

    move some component to hook

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,55 +1,72 @@
-import React, { Component } from 'react';
+import React from "react";
 
-class Header extends Component {
-  render() {
+const NAVS = [
+  { label: "Home", hash: "#home", current: true },
+  { label: "About", hash: "#about", current: false },
+  { label: "Resume", hash: "#resume", current: false },
+  { label: "Works", hash: "#portfolio", current: false },
+  { label: "Testimonials", hash: "#testimonials", current: false },
+  { label: "Contact", hash: "#contact", current: false },
+];
 
-    if(this.props.data){
-      var name = this.props.data.name;
-      var occupation= this.props.data.occupation;
-      var description= this.props.data.description;
-      var city= this.props.data.address.city;
-      var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
-      })
-    }
+const Header = ({ data }) => {
+  if (!data) return null;
 
-    return (
-      <header id="home">
+  const {
+    name,
+    occupation,
+    description,
+    address: { city },
+    social,
+  } = data;
 
+  return (
+    <header id="home">
       <nav id="nav-wrap">
+        <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+          Show navigation
+        </a>
+        <a className="mobile-btn" href="#home" title="Hide navigation">
+          Hide navigation
+        </a>
 
-         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
-
-         <ul id="nav" className="nav">
-            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-            <li><a className="smoothscroll" href="#about">About</a></li>
-	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
-            <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-            <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
-         </ul>
-
+        <ul id="nav" className="nav">
+          {NAVS.map(({ label, hash, current }) => (
+            <li className={current && "current"} key={label}>
+              <a className="smoothscroll" href={hash}>
+                {label}
+              </a>
+            </li>
+          ))}
+        </ul>
       </nav>
 
       <div className="row banner">
-         <div className="banner-text">
-            <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
-            <hr />
-            <ul className="social">
-               {networks}
-            </ul>
-         </div>
+        <div className="banner-text">
+          <h1 className="responsive-headline">I'm {name}.</h1>
+          <h3>
+            I'm a {city} based <span>{occupation}</span>. {description}.
+          </h3>
+          <hr />
+          <ul className="social">
+            {social.map((network) => (
+              <li key={network.name}>
+                <a href={network.url}>
+                  <i className={network.className} />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
 
       <p className="scrolldown">
-         <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+        <a className="smoothscroll" href="#about">
+          <i className="icon-down-circle" />
+        </a>
       </p>
-
-   </header>
-    );
-  }
-}
+    </header>
+  );
+};
 
 export default Header;
