31844074c309dd81bcebdd01389b3e3dd5c1b621
Angus Hsu <apangus611@gmail.com>
2022-06-29 12:05:33 +0800

    update CV

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -43,7 +43,7 @@ const About = ({ data }) => {
             <div className="columns download">
               <p>
                 <a
-                  href="https://line-bot-pictures-angushsu.s3.ap-northeast-1.amazonaws.com/Angus's+CV.pdf"
+                  href="https://line-bot-pictures-angushsu.s3.ap-northeast-1.amazonaws.com/Angus+Hsu+CV.pdf"
                   className="button"
                   target="_blank"
                 >
